26afcdcedd2eb62d182fa41d362b482744c8ac2d
Prashant <r56.prashant@gmail.com>
2024-08-15 21:53:17 +0545

    checkpoint

---
+++ src/script/script.js
@@ -0,0 +1,14 @@
+//Creating elements
+
+
+//select the <ul></ul> element
+const ul = document.querySelector('ul')
+
+
+//Create the element <li></li>
+const li = document.createElement('li')
+
+
+//Add the created element
+
+ul.append(li)
