0b6d8cf74b28286358f3722cb211e592ed8327f7
Prashant <r56.prashant@gmail.com>
2024-08-15 22:16:15 +0545

    creation removal and modifying

--- src/script/script.js
+++ src/script/script.js
@@ -12,3 +12,34 @@ const li = document.createElement('li')
 //Add the created element
 
 ul.append(li)
+
+//Modifying content
+
+const firstItem = document.querySelector(".list-item")
+console.log(firstItem)
+
+console.log(firstItem.innerText)
+console.log(firstItem.textContent)
+console.log(firstItem.innerHTML)
+
+
+li.innerText = "I am the new list"
+
+//Modifying attributes (attribute, value)
+
+li.setAttribute('class',"list-item")
+
+//li.removeAttribute()
+
+//Get attribute
+
+console.log(li.getAttribute('class'))
+
+
+// For classes (add, remove, toggle, replace, contains)
+//li.classList.add("new-class")
+
+
+//Remove an element
+
+li.remove();
